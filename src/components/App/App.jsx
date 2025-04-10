233a2a4ae827ad21107091e3ceead8128935a23b
Serhii-Yakimtso <serafyso@gmail.com>
2025-04-10 21:14:50 +0300

    add router-dom

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import Home from '../../pages/Home/Home';
+import About from '../../pages/About/About';
+import NotFound from '../../pages/NotFound/NotFound';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <>
+      <h1>Not Found Page</h1>
+      <p>Oops!</p>
+    </>
+  );
+}
+
+export default NotFound;
